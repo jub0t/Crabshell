fbcbff9a526c55bcdb8dc02e70b2d6a72ade1d8c
Jareer <jub0t@proton.me>
2024-10-06 23:29:55 +0500

    Okay, let's figure out tonic first

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,14 @@ pub mod bot;
 pub mod core;
 pub mod utils;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::configure()
+        .build_server(true)
+        .compile(&["proto/bot/bot.proto"], &["proto/bot"])?;
+
     // Let's start the gRPC("core") client for out-going and in-coming communications.
     core::start();
     utils::process::prevent_death();
+
+    Ok(())
 }
