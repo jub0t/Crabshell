c935fd81fa8b156660ee287cf7987503d61d34e7
Jareer <jub0t@proton.me>
2024-10-11 16:27:43 +0500

    Build.rs works

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,5 +1,30 @@
 use std::{process::exit, thread, time::Duration};
 
+use tonic::{transport::Server, Request, Response, Status};
+
+use bot::{StartRequest, StartResponse};
+
+#[derive(Debug, Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl A for MyGreeter {
+    async fn say_hello(
+        &self,
+        request: Request<StartRequest>, // Accept request of type HelloRequest
+    ) -> Result<Response<StartResponse>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = StartResponse { success: true };
+
+        Ok(Response::new(reply)) // Send back our formatted greeting
+    }
+}
+
+pub mod bot {
+    tonic::include_proto!("bot"); // The string specified here must match the proto package name
+}
+
 pub fn start() {
     let _t = thread::spawn(move || {});
 }
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,6 @@ pub mod core;
 pub mod utils;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .build_server(true)
-        .compile(&["proto/bot/bot.proto"], &["proto/bot"])?;
-
     // Let's start the gRPC("core") client for out-going and in-coming communications.
     core::start();
     utils::process::prevent_death();
