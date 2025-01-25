4d36de20507caebad217a62934b8257ebe6dd1a5
Jareer <jforeverything2007@gmail.com>
2025-01-25 21:25:39 +0500

    better structure

---
+++ src/storage/go.rs
@@ -0,0 +1,9 @@
+// Implementation for Golang application functions
+// GO
+
+use super::Storage;
+
+impl Storage {
+    // TODO: initialize golang project directory.
+    pub fn initialize_go() {}
+}
---
+++ src/storage/js.rs
@@ -0,0 +1,12 @@
+// Implementation for Javascript-based application functions
+// NODE.JS, DENO, BUN.JS
+
+use super::Storage;
+
+impl Storage {
+    // TODO: Do this.
+    pub fn install_node_modules() {}
+
+    // npm init --yes or use custom json
+    pub fn initialize_node() {}
+}
