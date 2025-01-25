7eb5d948b8130f5f941475682996609384faefef
Jareer <jforeverything2007@gmail.com>
2025-01-25 21:27:06 +0500

    Update process.rs

--- src/utils/process.rs
+++ src/utils/process.rs
@@ -1,5 +1,9 @@
+use std::{thread::sleep, time::Duration};
+
 // Function to prevent the main process from exiting
 // TODO: Add thread sleep or user input with interval so the loop doesn't use to much cpu
 pub fn prevent_death() {
-    loop {}
+    loop {
+        sleep(Duration::from_secs(60));
+    }
 }
